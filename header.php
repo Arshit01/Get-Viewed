633a8cc5684876a0d0b046d2e54cac3f663e7ee5
Arshit01 <arshishav@gmail.com>
2022-06-30 14:58:04 +0530

    Starting.

---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- JavaScript -->
+
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/header.css" type="text/css">
+</head>
+
+<body>
+    <!-- header -->
+    <nav>
+        <div class="navbar">
+            <!-- logo -->
+            <!-- <div class="logo">
+                <a href="index.html"><img src="logo.png" alt="Logo"></a>
+            </div> -->
+            <!-- logo -->
+            <!-- navigation -->
+            <div class="toggler">
+                <ul class="nav-list">
+                    <li class="nav-list-item"><a href="#">Home</a></li>
+                    <li class="nav-list-item"><a href="#">About Us</a></li>
+                    <li class="nav-list-item"><a href="#">Pricing</a></li>
+                    <li class="nav-list-item"><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <!-- //navigation -->
+        </div>
+    </nav>
+    <!-- //header -->
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Get-Viewed</title>
+
+    <!-- JavaScript -->
+    <script src="C:\xampp\htdocs\Get-viewed\js\bootstrap.min.js" type="text/javascript"></script>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+
+</head>
+
+<body>
+    <?php
+		include 'header.php';
+	 ?>
+</body>
+
+</html>
